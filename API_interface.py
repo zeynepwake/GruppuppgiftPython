7fbd9f11151ef97a66213ec05c57f79fed1ea120
Jakob Zohlen <jakob.zohlen@gmail.com>
2021-09-20 22:27:16 +0200

    First draft of API interface class

---
+++ API_interface.py
@@ -0,0 +1,81 @@
+import requests
+
+class API_Requests:
+
+    url = 'http://localhost:8000'
+    models = [ 'question_answering', 'text_generator', 'sentiment_analysis', 'image_classifier' ]
+    current_model = None
+
+    #--------------------------------------------------------#
+    #Methods for starting different models from the API
+    #--------------------------------------------------------#
+    def _make_request(model):
+            r = requests.post(API_Requests.url + '/start', json = { 'name' : model})
+            API_Requests.current_model = model
+            return r
+
+    def start_qa_model():
+        return API_Requests._make_request(API_Requests.models[0])
+
+    def start_generator_model():
+        return API_Requests._make_request(API_Requests.models[1])
+
+    def start_sentiment_model():
+        return API_Requests._make_request(API_Requests.models[2])
+
+    def start_image_model():
+        return API_Requests._make_request(API_Requests.models[3])
+    #--------------------------------------------------------#
+    #--------------------------------------------------------#
+
+    def post_qa(context: str, question: str) -> requests.models.Response:
+        """Makes a post request to the API for the question/answer model
+
+        Args:
+            context (str): The context for the question to be answered
+            question (str): The question
+
+        Returns:
+            requests.models.Response: An instance of the Response type, 
+            containing a JSON method to extract the answer from the model
+        """
+        if API_Requests.current_model == None:
+            print('Select a model')
+            return
+        data = {"context": context, "question": question}
+        r = requests.post(API_Requests.url + '/qa', json=data)
+        return r
+
+    def post_generator(context: str) -> requests.models.Response:
+        """Makes a post request to the API for the text generation model
+
+        Args:
+            context (str): The context for the text being generated
+
+        Returns:
+            requests.models.Response: An instance of the Response type, 
+            containing a JSON method to extract the answer from the model
+        """
+        if API_Requests.current_model == None:
+            print('Select a model')
+            return
+        data = {"context": context}
+        r = requests.post(API_Requests.url + '/text_generation', json=data)
+        return r
+
+    def post_sentiment(context: str) -> requests.models.Response:
+        """Makes a post request to the API for the sentiment analysis model
+
+        Args:
+            context (str): The context for the evaluation
+
+        Returns:
+            requests.models.Response: Response: An instance of the Response type, 
+            containing a JSON method to extract the answer from the model
+        """
+        if API_Requests.current_model == None:
+            print('Select a model')
+            return
+        data = {"context": context}
+        r = requests.post(API_Requests.url + '/sentiment_analysis', json=data)
+        return r
