712117116908f404045b9c1251dabb8616160058
zeynepwake <89340864+zeynepwake@users.noreply.github.com>
2021-09-20 18:55:03 +0200

    Update gruppstream.py

--- gruppstream.py
+++ gruppstream.py
@@ -4,5 +4,6 @@ st.write('Hello and Welcome!')
 user_name = st.text_input('What shall we call you?')
 st.button('Enter and Save')
 st.write('Hello', user_name,'!')
-user_input= st.selectbox('Please select a model', ['Text Generator', 'Sentimental Analysis', 'QA'])
+#user_input= st.selectbox('Please select a model', ['Text Generator', 'Sentimental Analysis', 'QA'])
+user_input= st.multiselect('Please select a model', ['Text Generator', 'Sentimental Analysis', 'QA'], ['Text Generator'])
 
