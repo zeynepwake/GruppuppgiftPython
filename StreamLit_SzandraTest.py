422bce7dd4fcc2830f213b9b643eba29c39c6a72
Szandra Varga <varga.szandra@gmail.com>
2021-09-24 16:50:55 +0200

    StreamLit TEST - Sandbox only (Under Construction)

--- StreamLit_SzandraTest.py
+++ StreamLit_SzandraTest.py
@@ -1,14 +1,17 @@
 import streamlit as st
+from st_aggrid import AgGrid
 from PIL import Image
 import DB_Handling
+from API_interface import API_Requests
 
+st.set_page_config(layout="wide")
 page = st.sidebar.selectbox('Vilken sida vill du se',
                     ['Registration', 'Login'])
+
 if page == 'Registration':
     ML_db = DB_Handling.MLModel_DB('DB_ML_History.db')
     st.header('Du är på Registration Sida')
-    #Här kommer alla kod för Sida 1
-    with st.form('This is a Regform'):
+    with st.form('Registration form'):
         col1, col2 = st.columns(2)
         with col1:
             first_name = st.text_input('Enter your first name')
@@ -18,13 +21,35 @@ if page == 'Registration':
             password = st.text_input('Choose a password')
         
         doit = st.form_submit_button('Submit :)') # Ez csak a formon belul mukodik
-    if doit:     
-        if ML_db.chk_account_if_exists(user_name):
-            st.write('Accountname exists. Try another one.')
-        else: 
-            ML_db.create_user(user_name, password, first_name, last_name)
-            st.write('You are registered. Please Log In')
+        if doit:     
+            if ML_db.chk_account_if_exists(user_name):
+                st.write('Accountname exists. Try another one.')
+            else: 
+                ML_db.create_user(user_name, password, first_name, last_name)
+                st.write('You are registered. Please Log In')
 
-else:
-    st.write('Du är på Login')
-    #Här kommer alla kod för Sida 2
+else: #LOGIN Section
+    ML_db = DB_Handling.MLModel_DB('DB_ML_History.db')
+    st.header('Du är på Login Sida')
+    with st.form('Login form'):
+        user_name = st.text_input('Please enter your user name')
+        password = st.text_input('Please give you password')
+        doit = st.form_submit_button('Submit :)')        
+        if doit:     
+            if ML_db.chk_account_if_exists(user_name):
+                df_user = ML_db.login(user_name, password)
+                if df_user.empty:
+                    st.write ('Try again') 
+                else :
+                    models = API_Requests.models
+                    chosen_model= st.selectbox('Please select a model', models)
+                    if not chosen_model == 'question_answering':
+                        user_context = st.text_input('Please type below')
+                        user_question= None
+                        send_to_API = st.form_submit_button('Send')
+                    else:
+                        user_context = st.text_input('Please type below')
+                        user_question= st.text_input('.. and the question?')
+                        send_to_API = st.form_submit_button('Send')
+            else:
+                st.write('This account name does not exists. Tyr with another one, please')
